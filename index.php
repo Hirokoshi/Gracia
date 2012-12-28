73a6164dbf7412feb8b95ecdd4558f9fb063631a
Elyas Kamel <melyasfa@gmail.com>
2012-12-28 12:59:19 +0100

    Upgrade README.md and set a new example.

--- index.php
+++ index.php
@@ -7,10 +7,11 @@ spl_autoload_register('loadClass');
 
 //EXAMPLE
 $obj = new Gracia("myPicture", 350, 150);
+$obj->setFont('AlteHaasGroteskRegular.ttf');
 $obj->setBackground("#8c6430");
-$obj->setLine(10, 10, 60, 60, "#fff");
-$obj->rotate(180);
-$obj->setBorder("#000000", 3);
+$obj->setTtfText(14, 120, 30, "Hello world!", "#abcdef");
+$obj->setLine(120, 40, 220, 40, "#abcdef");
+$obj->setBorder("#abcdef", 3);
 $obj->show_img();
 ?>
 
